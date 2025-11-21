9d3ca96e2ac6ae0876f231feff917226e01966c5
Dhanveer Ramnauth <dhanr1606@gmail.com>
2025-11-21 13:12:02 +1100

    idk

--- init.lua
+++ init.lua
@@ -4,9 +4,12 @@ require 'config.lazy'
 
 -- Map <Leader>j to move to the start of the line
 vim.api.nvim_set_keymap('n', '<Leader>j', '_', { noremap = true })
+vim.api.nvim_set_keymap('v', '<Leader>j', '_', { noremap = true })
 
 -- Map <Leader>h to move to the end of the line
 vim.api.nvim_set_keymap('n', '<Leader>k', '$', { noremap = true })
+vim.api.nvim_set_keymap('v', '<Leader>k', '$', { noremap = true })
+
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.g.omni_sql_no_default_maps = 1
 -- Define autocmd to set autoindent for PHP files
